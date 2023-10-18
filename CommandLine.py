1cf511d0b0d3edec11ea9cf75d18d7f8c1862122
Jasper Snoeks <j_snoeks@hotmail.be>
2023-10-18 10:25:46 +0200

    CPLM_positions
    create a dict with all acetylation positions for each unique CPLM id and create a csv with only unique CPLM ids

--- CommandLine.py
+++ CommandLine.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import Querry as q
+import Create_CPLMdf as c
 
 print("Welcome to the protein acetylation website!\n")
 
-CPLM_Database = pd.read_csv('C:/Users/friga/Desktop/VSCode/data.csv')
+CPLM_positions = c.get_CPLM_data('Acetylation.txt')
+print(CPLM_positions)
+CPLM_Database = pd.read_csv('data.csv')
 
-print(q.Querry(CPLM_Database))
+#print(q.Querry(CPLM_Database))
--- Create_CPLMdf.py
+++ Create_CPLMdf.py
@@ -4,16 +4,40 @@ import pandas as pd
 ### Create the Acetylation Database from the CPLM txt file###
 #############################################################
 
-#This file creates a csv folder from the initial txt from cplm
-filepath = '3rd Semester/Project/Acetylation.txt'
+def get_CPLM_data(path):
 
-#CPLM_initial_dataframe = pd.read_csv(filepath, delimiter='\t')
-CPLM_initial_dataframe = pd.read_table(filepath, delimiter='\t', header=None)
-
-#Only keep the useful information of the initial array
-final_CPLM_dataframe=CPLM_initial_dataframe.iloc[:,[0,1,2,4,5,6]]
-#print(final_CPLM_dataframe.iloc[200,0])
-#print(final_CPLM_dataframe.iloc[:,[0,1,2,3,4]]) #column 5 is the sequence
-final_CPLM_dataframe.columns = ["CPLM_id", "Accession_Number", "Position", "Protein_Name","Species","Sequence"]
-
-final_CPLM_dataframe.to_csv('data.csv', index=False)
+    #This file creates a csv folder from the initial txt from cplm
+    filepath = path
+    
+    #CPLM_initial_dataframe = pd.read_csv(filepath, delimiter='\t')
+    CPLM_initial_dataframe = pd.read_table(filepath, delimiter='\t', header=None)
+    
+    #Only keep the useful information of the initial array
+    df=CPLM_initial_dataframe.iloc[:,[0,1,2,4,5,6]]
+    #print(final_CPLM_dataframe.iloc[200,0])
+    #print(final_CPLM_dataframe.iloc[:,[0,1,2,3,4]]) #column 5 is the sequence
+    df.columns = ["CPLM_id", "Accession_Number", "Position", "Protein_Name","Species","Sequence"]
+    
+    duplicated_first=df.duplicated(subset=["CPLM_id"])
+    CPLM_positions = {}
+    positions = []
+    ii = 0
+    for i in df.index:
+        
+        if duplicated_first[i]==False:
+            CPLM_positions[df["CPLM_id"][ii]] = positions
+            ii = i
+            positions = []
+        
+        positions.append(df["Position"][i])
+    CPLM_positions[df["CPLM_id"][ii]] = positions   
+      
+        
+    #print(CPLM_positions)
+    
+    dff = df.drop_duplicates(subset=["CPLM_id"])
+    dff = dff.drop(columns=["Position"])
+    
+    dff.to_csv('data.csv', index=False)
+    
+    return CPLM_positions
