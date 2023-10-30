545c517aace52118c125426869eaf3e5030a0bac
FotisRigas1261 <fotios-filippos.rigas@student.kuleuven.be>
2023-10-31 00:14:27 +0200

    Accessibility, integrated

--- CommandLine.py
+++ CommandLine.py
@@ -4,22 +4,7 @@ import sys
 import get_from_uniprot
 import alphafold
 import Lysine_acetylation_conservation as lys
-#import Secondary_structure_json as secnd
-#import pdfReportCreator
-
-#Trial with structure map
-# Import structuremap functions
-# import structuremap.utils
-# structuremap.utils.set_logger()
-# from structuremap.processing import download_alphafold_cif, download_alphafold_pae, format_alphafold_data, annotate_accessibility, get_smooth_score, annotate_proteins_with_idr_pattern, get_extended_flexible_pattern, get_proximity_pvals, perform_enrichment_analysis, perform_enrichment_analysis_per_protein, evaluate_ptm_colocalization, extract_motifs_in_proteome
-# from structuremap.plotting import plot_enrichment, plot_ptm_colocalization
-# import pandas as pd
-# import numpy as np
-# import os
-# import re
-# import plotly.express as px
-# import tqdm
-# import tempfile
+import accessibility as acc
 
 
 #Find the querried string
@@ -49,28 +34,18 @@ Uniprot_id_of_Querry,Lysine_positions=q.Querry(Querry_string)
 get_from_uniprot.get_uniprot_fasta(Uniprot_id_of_Querry)
 get_from_uniprot.print_data(Uniprot_id_of_Querry)
 
-##2.PDB files from alphafold
-#link = alphafold.get_alphafold_download_link(Uniprot_id_of_Querry)
-#print(link)
-#alphafold.parse_pdb(link)
+##2. Secondary structure
+acc.get_residue_accesibility(Uniprot_id_of_Querry)
 
 ##3.Conservation-works but slowly
 #lys.run_blast(Uniprot_id_of_Querry)
-#print(lys.conservation_score(Uniprot_id_of_Querry,Lysine_positions))
+print(lys.conservation_score(Uniprot_id_of_Querry,Lysine_positions))
 
-##4. Files from uniprot
+##4. Get gff
 #print(get_from_uniprot.get_uniprot_gff(Uniprot_id_of_Querry))
-#get_from_uniprot.function_site(Uniprot_id_of_Querry)
+#WORKS print(get_from_uniprot.function_site(Uniprot_id_of_Querry)) #This has to be parsed to create a dataframe
 #get pdb files:
 #get_from_uniprot.get_uniprot_pdb(Uniprot_id_of_Querry)
 
-##5.Secondary structure
-#print(secnd.helix_positions(Uniprot_id_of_Querry))
-#print(secnd.beta_positions(Uniprot_id_of_Querry))
 
-# alphafold_annotation = format_alphafold_data(
-#     directory="C:/Users/friga/Desktop/VSCode/IBPproject", 
-#     protein_ids=Uniprot_id_of_Querry)
 
-##END: Create a report
-#pdfReportCreator.createreport(Uniprot_id_of_Querry,FASTA)
--- accessibility.py
+++ accessibility.py
@@ -1,58 +1,82 @@
-# import numpy as np
-# import structuremap.utils
-# structuremap.utils.set_logger()
-# from structuremap.processing import download_alphafold_cif, download_alphafold_pae, format_alphafold_data, annotate_accessibility, get_smooth_score, annotate_proteins_with_idr_pattern, get_extended_flexible_pattern, get_proximity_pvals, perform_enrichment_analysis, perform_enrichment_analysis_per_protein, evaluate_ptm_colocalization, extract_motifs_in_proteome
-# import os
+import numpy as np
+import structuremap.utils
+structuremap.utils.set_logger()
+from structuremap.processing import download_alphafold_cif, download_alphafold_pae, format_alphafold_data, annotate_accessibility, get_smooth_score, annotate_proteins_with_idr_pattern, get_extended_flexible_pattern, get_proximity_pvals, perform_enrichment_analysis, perform_enrichment_analysis_per_protein, evaluate_ptm_colocalization, extract_motifs_in_proteome
+import os
+import shutil
 
 
 
-# output_dir = r"C:\Users\friga\Desktop\VSCode\IBPproject"
+output_dir = r"C:\Users\friga\Desktop\VSCode\IBPproject"
 
-# cif_dir = os.path.join(output_dir, 'acetylation_cif')
-# pae_dir = os.path.join(output_dir, 'acetylation_pae')
-
-
-# def get_residue_accesibility(uniprot_ID):
-    
-    
-#     valid_proteins_cif, invalid_proteins_cif, existing_proteins_cif = download_alphafold_cif(
-#     proteins=uniprot_ID,
-#     out_folder=cif_dir)
-    
-    
-    
-#     valid_proteins_pae, invalid_proteins_pae, existing_proteins_pae = download_alphafold_pae(
-#     proteins=uniprot_ID,
-#     out_folder=pae_dir, 
-#     )
-    
-#     alphafold_annotation = format_alphafold_data(
-#     directory=cif_dir, 
-#     protein_ids=uniprot_ID)
-    
-#     full_sphere_exposure = annotate_accessibility(
-#     df=alphafold_annotation, 
-#     max_dist=24, 
-#     max_angle=180, 
-#     error_dir=pae_dir)
-    
-#     alphafold_accessibility = alphafold_annotation.merge(
-#     full_sphere_exposure, how='left', on=['protein_id','AA','position'])
-    
-    
-#     alphafold_accessibility_smooth = get_smooth_score(
-#     alphafold_accessibility, 
-#     np.array(['nAA_24_180_pae']), 
-#     [10])
-    
-#     alphafold_accessibility_smooth['IDR'] = np.where(
-#     alphafold_accessibility_smooth['nAA_24_180_pae_smooth10']<=34.27, 1, 0)
-    
-#     return alphafold_accessibility_smooth
+cif_dir = os.path.join(output_dir, 'acetylation_cif')
+pae_dir = os.path.join(output_dir, 'acetylation_pae')
 
 
+def get_residue_accesibility(id_from_uniprot):
+    
+    #The functions used afterwards take only lists as arguments
+    uniprot_ID=[id_from_uniprot]
 
-# access = get_residue_accesibility(['O00115'])
+    # #Got these from outside
+    # output_dir = r"C:\Users\friga\Desktop\VSCode\IBPproject"
 
+    # cif_dir = os.path.join(output_dir, 'acetylation_cif')
+    # pae_dir = os.path.join(output_dir, 'acetylation_pae')
+    # ####
+    
+    valid_proteins_cif, invalid_proteins_cif, existing_proteins_cif = download_alphafold_cif(
+    proteins=uniprot_ID,
+    out_folder=cif_dir)
+    
+    
+    
+    valid_proteins_pae, invalid_proteins_pae, existing_proteins_pae = download_alphafold_pae(
+    proteins=uniprot_ID,
+    out_folder=pae_dir, 
+    )
+    
+    alphafold_annotation = format_alphafold_data(
+    directory=cif_dir, 
+    protein_ids=uniprot_ID)
+    
+    full_sphere_exposure = annotate_accessibility(
+    df=alphafold_annotation, 
+    max_dist=24, 
+    max_angle=180, 
+    error_dir=pae_dir)
+    
+    alphafold_accessibility = alphafold_annotation.merge(
+    full_sphere_exposure, how='left', on=['protein_id','AA','position'])
+    
+    
+    alphafold_accessibility_smooth = get_smooth_score(
+    alphafold_accessibility, 
+    np.array(['nAA_24_180_pae']), 
+    [10])
+    
+    alphafold_accessibility_smooth['IDR'] = np.where(
+        #The threshold for intrinsicly disordered protein is set at 34.27, this needs further investigation
+    alphafold_accessibility_smooth['nAA_24_180_pae_smooth10']<=34.27, 1, 0)
+    
+    #Remove excess files 
+    directory_path = r'C:\Users\friga\Desktop\VSCode\IBPproject'
+    file_list = os.listdir(directory_path)
+    filenames_to_Remove = ["acetylation_cif", "acetylation_pae"]
+    for filename in file_list:
+        if any(keyword in filename for keyword in filenames_to_Remove):
+            file_path = os.path.join(directory_path, filename)
+            try:
+                shutil.rmtree(file_path)
+                print(f"Removed file: {file_path}")
+            except Exception as e:
+                print(f"Error removing file {file_path}: {e}")
 
-# print(access)  
+    #Store the dataframe as a csv file
+    csv_file_path = 'SecondaryStrAndAccessibility.csv'
+
+    # Save the DataFrame to a CSV file
+    alphafold_accessibility_smooth.to_csv(csv_file_path, index=False)
+
+    return alphafold_accessibility_smooth
+
