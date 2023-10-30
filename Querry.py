9defd207e6227b5ab6ef1ab75c638e97aceb1889
FotisRigas1261 <121730750+FotisRigas1261@users.noreply.github.com>
2023-10-30 22:49:47 +0200

    Update Querry.py hope final this time

--- Querry.py
+++ Querry.py
@@ -23,8 +23,6 @@ def Querry(Querry_string):
                                 index = int(input("Type the number corresponding to your desired id: ".format(len(CPLMids))))
                                 if 1 <= index <= len(CPLMids):
                                     item_to_keep = CPLMids[index - 1]
-
-
                                     #Add the code from CPLM querry:
                                     with open('../CPLMids.txt', 'r') as file:
                                         file_contents = file.read()
@@ -34,11 +32,22 @@ def Querry(Querry_string):
                                         UniprotID_list = [key for key, value in data_dict.items() if item_to_keep in value]
                                         #Set this to querry string so that it gets returned at the end
                                         Querry_string = UniprotID_list[0]
-                                    print(UniprotID)
+                                    print(Querry_string)
                                 else:
                                     print("Index out of bounds. Please enter a valid index.")
                             except:
                                 print("Invalid input. Please enter a valid integer index.")
+
+                                ###NEW CODE here for list containing only one id
+                        if len(CPLMids)==1:
+                            with open('../CPLMids.txt', 'r') as file:
+                                        file_contents = file.read()
+                                        #Package ast is used to handle txt files as dictionaries
+                                        data_dict = ast.literal_eval(file_contents)  # Safely parse the dictionary
+                                        #This creates a list which should only contain one element
+                                        UniprotID_list = [key for key, value in data_dict.items() if CPLMids[0] in value]
+                                        #Set this to querry string so that it gets returned at the end
+                                        Querry_string = UniprotID_list[0]
                     except:
                         print("Protein not found!")
 
@@ -73,5 +82,3 @@ def Querry(Querry_string):
     else:
         #In this case the querry is already the Uniprot ID
         return Querry_string,positions
-
- 
