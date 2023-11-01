3b767dd03a5eb255d7f53b88d5494c5dae3d2fed
FotisRigas1261 <fotios-filippos.rigas@student.kuleuven.be>
2023-11-01 20:52:11 +0200

    Update parse gff, it should work with all gff file

--- parse_gff.py
+++ parse_gff.py
@@ -11,71 +11,79 @@ def parse_gff(UniprotGff):
     natural_variants=[]
     try:
         with open(UniprotGff, "r") as file:
+            line_count = 0
+
             for line in file:
+                line_count += 1
+            if line_count > 2:
+                #return to the begining of the file
+                file.seek(0)
+                for line in file:
                 #####################################
                 #First, creating mutations data frame
                 #####################################
                 #If the keyword is mutagenesis
-                if mut_keyword1 in line:
-                    #getting the positions
-                    columns = line.strip().split('\t')
-                    position = columns[3]
-                    #getting mutation type
-                    match1 = re.search(r'([A-Z])->([A-Z])', line)
-                    if match1:
-                        before = match1.group(1)
-                        after = match1.group(2)
-                        mtype = before + "->" + after
-                    #getting the effect
-                    match2 = re.search(r'Note=([^.%]+)[.%]', line)
-                    effect = match2.group(1)
-                    #getting the source
-                    match3 = re.search(r'PubMed:([^;]+)[;]', line)
-                    source = "PubMed:"+match3.group(1)
-
-                    mutations_data.append([position,mtype,effect,source])
-                    if len(mutations_data)>0:
-                        mutations_df = pd.DataFrame(mutations_data, columns=["Position","Type of mutation","Effect","Evidence"])
-
-                #if the keyword is Natural variant
-                if mut_keyword2 in line:
-                    #getting the positions
-                    columns = line.strip().split('\t')
-                    position = columns[3]
-                    #getting mutation type
-                    match1 = re.search(r'([A-Z])->([A-Z])', line)
-                    if match1:
-                        before = match1.group(1)
-                        after = match1.group(2)
-                        mtype = before + "->" + after
-                    #getting the effect
-                    match2 = re.search(r'dbSNP:([^.,]+)[.,]?', line)
-                    if match2:
+                    if mut_keyword1 in line:
+                        #getting the positions
+                        columns = line.strip().split('\t')
+                        position = columns[3]
+                        #getting mutation type
+                        match1 = re.search(r'([A-Z])->([A-Z])', line)
+                        if match1:
+                            before = match1.group(1)
+                            after = match1.group(2)
+                            mtype = before + "->" + after
+                        #getting the effect
+                        match2 = re.search(r'Note=([^.%]+)[.%]', line)
                         effect = match2.group(1)
-                    else:
-                        match2b = re.search(r'AIPCS%([^%.]+)[%.]', line)
-                        effect = match2b.group(1)
-                    #getting the source
-                    match3 = re.search(r'PubMed:([^;,]+)[;,]', line)
-                    if match3:
+                        #getting the source
+                        match3 = re.search(r'PubMed:([^;]+)[;]', line)
                         source = "PubMed:"+match3.group(1)
-                    else:
-                        source=" "
 
-                    natural_variants.append([position,mtype,effect,source])
-                    if len(natural_variants)>0:
-                        natural_variants_df = pd.DataFrame(natural_variants, columns=["Position","Type of variation","Effect","Evidence"])              
-                #######################################
-                #Second, creating structures data frame
-                #######################################
-                if not (mut_keyword1 in line or mut_keyword2 in line) and not line.startswith("#"):
-                    columns = line.strip().split('\t')
-                    start_position=columns[3]
-                    end_position=columns[4]
-                    structure=columns[2]
-                    structure_data.append([start_position,end_position,structure])
-                    if len(structure_data)>0:
-                        structures_df=pd.DataFrame(structure_data, columns=["Start position","End position","Structure"])
+                        mutations_data.append([position,mtype,effect,source])
+                        if len(mutations_data)>0:
+                            mutations_df = pd.DataFrame(mutations_data, columns=["Position","Type of mutation","Effect","Evidence"])
+
+                    #####################################
+                    #Creating Natural variant dataframe
+                    #####################################
+                    if mut_keyword2 in line:
+                        #getting the positions
+                        columns = line.strip().split('\t')
+                        position = columns[3]
+                        #getting mutation type
+                        match1 = re.search(r'([A-Z])->([A-Z])', line)
+                        if match1:
+                            before = match1.group(1)
+                            after = match1.group(2)
+                            mtype = before + "->" + after
+                        #getting the effect
+                        match2 = re.search(r'dbSNP:([^.,]+)[.,]?', line)
+                        if match2:
+                            effect = match2.group(1)
+                        else:
+                            match2b = re.search(r'AIPCS%([^%.]+)[%.]', line)
+                            effect = match2b.group(1)
+                        #getting the source
+                        match3 = re.search(r'PubMed:([^;,]+)[;,]', line)
+                        if match3:
+                            source = "PubMed:"+match3.group(1)
+                        else:
+                            source=" "
+                        natural_variants.append([position,mtype,effect,source])
+                        if len(natural_variants)>0:
+                            natural_variants_df = pd.DataFrame(natural_variants, columns=["Position","Type of variation","Effect","Evidence"])              
+                    #######################################
+                    #Second, creating structures data frame
+                    #######################################
+                    if not (mut_keyword1 in line or mut_keyword2 in line) and not line.startswith("#"):
+                        columns = line.strip().split('\t')
+                        start_position=columns[3]
+                        end_position=columns[4]
+                        structure=columns[2]
+                        structure_data.append([start_position,end_position,structure])
+                        if len(structure_data)>0:
+                            structures_df=pd.DataFrame(structure_data, columns=["Start position","End position","Structure"])
     except FileNotFoundError:
             print(f"File not found: {UniprotGff}")
 
