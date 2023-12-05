8730a0f4709749bdf73d16c1c2fcbe8e458dc4e1
Jasper Snoeks <j_snoeks@hotmail.be>
2023-12-05 13:32:36 +0100

    update test

--- AC3D/test.py
+++ AC3D/test.py
@@ -1,13 +1,15 @@
 import os
 import time
 from AC3D import PATH, CommandLine
+import traceback
 
 def run_CommandLine(key):
     try:
         CommandLine.main(key, False)
         return key, True
     except Exception as e:
-        return key, False, e
+        traceback_str = traceback.format_exc()
+        return key, False, e, traceback_str
     
 def test_all(x=1):
     with open(os.path.join(PATH.PATH().data_path, "CPLMids.txt"), "r") as f:
@@ -45,48 +47,48 @@ def test_all(x=1):
         print(f"succes/runs: {succes}/{runs}")
 
         
-        
 def test_errors():
     file_path = "testoutput.txt"
     output_keys = []
 
     with open(file_path, 'r') as file:
-        for line in file:
-            output_keys.append(line.strip())
-
-    total = len(output_keys)
-    succes = 0
-    errors = []
+        output_keys = [line.strip() for line in file]
 
     for n, key in enumerate(output_keys, start=1):
-        print(f"{n}/{total}: {key}")
+        print(f"{n}/{len(output_keys)}: {key}")
         try:
             result = run_CommandLine(key)
             if result[1]:
-                succes += 1
+                # Remove the key from the file when no error occurs
+                with open(file_path, 'w') as file:
+                    file.write('\n'.join(k for k in output_keys if k != key))
             else:
-                errors.append((key, result[2]))
+                if "No objects to concatenate" not in str(result[2]) and\
+                "All arrays must be of the same length" not in str(result[2]) and\
+                "'_atom_site.pdbx_sifts_xref_db_acc'" not in str(result[2]) and\
+                "Empty file." not in str(result[2]):
+                    raise Exception(f"An error occurred for key {key}: {result[2]}\n{result[3]}")
 
-        except Exception as e:
-            print(f"An error occurred for key {key}: {e}")
-            errors.append((key, e))
+        except:
+            traceback.print_exc()  # This will print the traceback information
+            break
 
-    runs = succes + len(errors)
-    print(f"succes/runs: {succes}/{runs}")
+
 
 
 if __name__ == "__main__":
-    test_all(2064)
+    #test_all(18807)
+    test_errors()
         
-        #not fixed
-        #CommandLine.main("O00139-2", False) #accessibiltity 
-        #CommandLine.main("O08550", False) #accessibility
-        #CommandLine.main("O08710", False) 
-        #CommandLine.main("O14686", False) 
-        #CommandLine.main("O15018", False) 
-        #CommandLine.main("O15050", False) 
-        #CommandLine.main("O15078", False) 
-        #CommandLine.main("O15417", False) 
+        #not fixed all in accessiblity.py line 38
+        #CommandLine.main("O00139-2", False) #accessibiltity, No objects to concatenate
+        #CommandLine.main("O08550", False) #accessibility, No objects to concatenate
+        #CommandLine.main("P0CE54", False) #accessibility, All arrays must be of the same length
+        #CommandLine.main("P0CE55", False) #accessibility, '_atom_site.pdbx_sifts_xref_db_acc'
+        #CommandLine.main("P0CE57", False) #accessibility, Empty file.
+        #CommandLine.main("", False) 
+        #CommandLine.main("", False) 
+        #CommandLine.main("", False) 
         #CommandLine.main("", False) 
         
         #fixed
